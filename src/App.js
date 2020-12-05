5024023ec58254438ba74e829f82d4f8a052f2fc
Engr-Asad-Hussain <asad.h1998@yahoo.com>
2020-12-05 11:24:56 +0000

    Added text in App.txt

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Asad Hussain.
+          Edit <code>src/App.js</code> Asad Hussain. <br />
+          NED University of Engineering and Technology
         </p>
         <a
           className="App-link"
